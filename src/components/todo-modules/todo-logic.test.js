feaf18f199976187d0430b24e21e73ace40236ec
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2021-03-11 20:14:12 +0100

    test passed

--- src/components/todo-modules/todo-logic.test.js
+++ src/components/todo-modules/todo-logic.test.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import Logic from './todo-logic';
 
 test('if add is defined', () => {
-  document.body.innerHTML ='<ol></ol>'; 
+  document.body.innerHTML = '<ol></ol>';
   const logic = new Logic($('input'));
   logic.add();
   const removeButton = $('.remove_button').length;
